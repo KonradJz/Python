b797d2e29164ff0bf844a405ae1a18ec3ff93ef2
Konrad <Kondzio1231234@gmail.com>
2020-02-26 11:07:59 +0100

    Initial Commit

---
+++ start.py
@@ -0,0 +1,33 @@
+# print("Hello World")
+# a = 'Ala'
+# b = 'Marek'
+# c = 'Darek'
+# print(a)
+# print(b)
+# print(c)
+
+# dlugi_tekst = """Ala ma kota
+# a kot jest głodny."""
+# print(dlugi_tekst)
+
+
+# #def funkcja():
+# #    """ dokumentacja """
+# print(type(a))
+# print(a+b)
+# print(2+3)
+# print(2-3)
+# print(2*3)
+# print(2/3)
+# print(2//3)
+# print(2%3)
+# wiek = 4
+# print(f"Ala ma {wiek} lata")
+# imie = 'Ala'
+# imie = imie.upper()
+
+# lista = [1,2,3,4,'elo',imie]
+# lista = list()
+# lista = list.append(1)
+# print(imie)
+# print(lista)
